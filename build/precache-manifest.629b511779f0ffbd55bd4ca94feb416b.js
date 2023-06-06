181e059a748ba09dc5f13b280a33765f4f3df0da
andraszyk1 <andraszyk1@gmail.com>
2023-06-06 22:25:59 +0200

    gitmodified

---
+++ build/precache-manifest.629b511779f0ffbd55bd4ca94feb416b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0becb72cd05ec04d13b",
+    "url": "./static/css/main.dbb02e0e.chunk.css"
+  },
+  {
+    "revision": "b0becb72cd05ec04d13b",
+    "url": "./static/js/main.9dfae172.chunk.js"
+  },
+  {
+    "revision": "4ac0c8f077360262eea0",
+    "url": "./static/js/runtime~main.277f7f35.js"
+  },
+  {
+    "revision": "895003b231d05c6b8f7e",
+    "url": "./static/css/2.29781adf.chunk.css"
+  },
+  {
+    "revision": "895003b231d05c6b8f7e",
+    "url": "./static/js/2.48445ce0.chunk.js"
+  },
+  {
+    "revision": "ca0ec56610fb7c98008e",
+    "url": "./static/js/3.f19b7c39.chunk.js"
+  },
+  {
+    "revision": "a37c2c3d17d4767353021f73bb8d7c9e",
+    "url": "./index.html"
+  }
+];
